4a04b9ed7bc9d34197a870713b97d06689938f01
Houda Cherkaoui <houda.cherkaoui@gmail.com>
2021-09-07 14:25:20 -0500

    Solve fibonacci recursively

--- recursion_fibonacci.rb
+++ recursion_fibonacci.rb
@@ -1,13 +1,21 @@
+# def fibonacci(n)
+#   sequence = []
+#   (0..n).each do |n|
+#       if n < 2
+#           sequence << n
+#       else
+#           sequence << sequence[-1] + sequence[-2]
+#       end
+#   end
+#   sequence.last
+# end
+
 def fibonacci(n)
-  sequence = []
-  (0..n).each do |n|
-      if n < 2
-          sequence << n
-      else
-          sequence << sequence[-1] + sequence[-2]
-      end
+  if n < 2
+      n
+  else
+      fibonacci(n-1) + fibonacci(n-2)
   end
-  sequence.last
 end
 
 puts fibonacci(20)
